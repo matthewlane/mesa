0de0468fe8cae6bd97210a4c0f23a687651052d7
Matt Lane <matthewlane@gmail.com>
2014-10-08 19:26:53 -0700

    Change text field to char type

--- statuses/models.py
+++ statuses/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Status(models.Model):
-    text = models.TextField()
+    text = models.CharField(max_length=160)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __unicode__(self):
