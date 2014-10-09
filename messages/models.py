80a82b9e9dcace8f8fab1dfd08d5d1b27ab63e74
Matt Lane <matthewlane@gmail.com>
2014-10-08 19:05:22 -0700

    Create Message model

--- messages/models.py
+++ messages/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Message(models.Model):
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __unicode__(self):
+        return self.text
