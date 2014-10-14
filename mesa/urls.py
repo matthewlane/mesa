bb373ec50e50e4be04d6c43bd74dbaec89cb99f4
Matt Lane <matthewlane@gmail.com>
2014-10-13 20:51:07 -0700

    Create app urls

--- mesa/urls.py
+++ mesa/urls.py
@@ -1,9 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-from statuses import views
-
 urlpatterns = patterns('',
-    url(r'^statuses/$', views.StatusView.as_view(), name='status-list'),
+    url(r'^', include('statuses.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
