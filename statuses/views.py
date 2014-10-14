6c043a58f907f7058b0f96792c02201ad299e10e
Matt Lane <matthewlane@gmail.com>
2014-10-13 20:55:49 -0700

    Create index page view

--- statuses/views.py
+++ statuses/views.py
@@ -1,7 +1,13 @@
+from django.views.generic.list import ListView
+
 from rest_framework import generics
 
 from .models import Status
 from .serializers import StatusSerializer
+
+class IndexView(ListView):
+    model = Status
+    template_name = "index.html"
 
 class StatusView(generics.ListCreateAPIView):
     model = Status
