f23bfe98b3b1e67168f9a2c07c669b5f607686f8
Matt Lane <matthewlane@gmail.com>
2014-10-13 21:24:59 -0700

    Fix index pattern

--- statuses/urls.py
+++ statuses/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import patterns, url
 from statuses import views
 
 urlpatterns = patterns('',
-    url(r'^', views.IndexView.as_view(), name='index'),
+    url(r'^$', views.IndexView.as_view(), name='index'),
     url(r'^statuses/$', views.StatusView.as_view(), name='status-list'),
 )
