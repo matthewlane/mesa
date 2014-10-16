7a9723e5f79eda4f75fa2a48c0abd7d7e9057610
Matt Lane <matthewlane@gmail.com>
2014-10-15 23:46:50 -0700

    Serve home page with generic class-based view

--- statuses/urls.py
+++ statuses/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls import include, patterns, url
+from django.views.generic.base import TemplateView
+
 from rest_framework.routers import DefaultRouter
 from statuses import views
 
@@ -6,6 +8,7 @@ router = DefaultRouter()
 router.register(r'statuses', views.StatusView)
 
 urlpatterns = patterns('',
-    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^$', TemplateView.as_view(template_name='statuses/status_list.html'),
+        name='index'),
     url(r'^api/', include(router.urls)),
 )
--- statuses/views.py
+++ statuses/views.py
@@ -1,12 +1,7 @@
-from django.views.generic.list import ListView
-
 from rest_framework import viewsets
 
 from .models import Status
 from .serializers import StatusSerializer
-
-class IndexView(ListView):
-    model = Status
 
 class StatusView(viewsets.ModelViewSet):
     model = Status
