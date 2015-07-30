778765fe8b665e26552e9da44acb675eaa6e7e4b
Matt Lane <matthewlane@gmail.com>
2015-07-30 15:57:53 -0700

    Change serializer base class

--- statuses/serializers.py
+++ statuses/serializers.py
@@ -2,7 +2,8 @@ from rest_framework import serializers
 
 from .models import Status
 
-class StatusSerializer(serializers.ModelSerializer):
+class StatusSerializer(serializers.HyperlinkedModelSerializer):
 
     class Meta:
         model = Status
+        fields = ('id', 'url', 'text', 'created_at')
