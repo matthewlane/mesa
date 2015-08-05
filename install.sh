3bb4ca3a7ba9c6507268e4b10ce6e13c669fdda3
Matt Lane <matthewlane@gmail.com>
2015-08-05 16:15:19 -0700

    Setup virtualenv

--- install.sh
+++ install.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 
-# pip and virtualenv/wrapper
+export PROJECT_NAME=mesa
+export WORKON_HOME=$HOME/.virtualenvs
+export VIRTUALENV_DIR=$WORKON_HOME/$PROJECT_NAME
+export VIRTUALENV=$VIRTUALENV_DIR/bin
+
+# pip and virtualenv
 if ! hash pip 2>/dev/null; then
     echo "Installing pip..."
     echo "------------------------------"
 
+    # install pip
     cd /tmp
     wget -O get-pip.py https://bootstrap.pypa.io/get-pip.py --no-check-certificate
     sudo -H python get-pip.py
 
-    # virtualenvwrapper
+    # create virtualenv for project
     sudo -H pip install virtualenvwrapper
+    mkdir -p $WORKON_HOME
+    virtualenv $VIRTUALENV_DIR
+    $VIRTUALENV/pip install -U setuptools
 
     echo "pip installed"
     echo "------------------------------"
