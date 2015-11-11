298ce553fc68a5a5895aba4b8c5f09ae29bfcb56
Matt Lane <matthewlane@gmail.com>
2015-11-10 17:31:51 -0800

    Test creating Status object

--- statuses/tests.py
+++ statuses/tests.py
@@ -1,5 +1,14 @@
 from django.test import TestCase
+from .models import Status
 
 class BasicTestCase(TestCase):
     def test_getting_root(self):
         self.client.get('/')
+
+class StatusTest(TestCase):
+    def setUp(self):
+        Status.objects.create(text="status test string")
+
+    def test_status_was_created(self):
+        status = Status.objects.get(text="status test string")
+        self.assertEqual(status.text, "status test string")
