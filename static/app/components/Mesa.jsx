094bfdcb5fc7dc70887e3df5c0a2eb8c2abb8ce6
Matt Lane <matthewlane@gmail.com>
2015-11-12 08:00:19 -0800

    Use uuid module

--- static/app/components/Mesa.jsx
+++ static/app/components/Mesa.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import uuid from 'uuid';
 import Form from './Form';
 import MessageList from './MessageList';
 
@@ -35,25 +36,9 @@ export default React.createClass({
     }
   },
 
-  uuid: function () {
-    var i, random;
-    var uuid = '';
-
-    for (i = 0; i < 32; i++) {
-      random = Math.random() * 16 | 0;
-      if (i === 8 || i === 12 || i === 16 || i === 20) {
-        uuid += '-';
-      }
-      uuid += (i === 12 ? 4 : (i === 16 ? (random & 3 | 8) : random))
-        .toString(16);
-    }
-
-    return uuid;
-  },
-
   handleSubmit: function(message) {
     var newMessage = {
-      uuid: this.uuid(),
+      uuid: uuid.v4(),
       text: message || (100 * Math.random()).toString(),
       created_at: new Date().toString()
     };
