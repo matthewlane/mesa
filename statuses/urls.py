5c989ddf7ae126ca5b02ea5c676b0c1b94fdc204
Matt Lane <matthewlane@gmail.com>
2015-11-19 11:06:24 -0800

    Create view with initial server data

--- statuses/urls.py
+++ statuses/urls.py
@@ -1,5 +1,4 @@
-from django.conf.urls import include, patterns, url
-from django.views.generic.base import TemplateView
+from django.conf.urls import include, url
 
 from rest_framework.routers import DefaultRouter
 from statuses import views
@@ -7,8 +6,7 @@ from statuses import views
 router = DefaultRouter()
 router.register(r'statuses', views.StatusView)
 
-urlpatterns = patterns('',
-    url(r'^$', TemplateView.as_view(template_name='statuses/status_list.html'),
-        name='index'),
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
     url(r'^api/', include(router.urls)),
-)
+]
--- statuses/views.py
+++ statuses/views.py
@@ -1,7 +1,20 @@
+from django.shortcuts import render
+
 from rest_framework import viewsets
+from rest_framework.renderers import JSONRenderer
 
 from .models import Status
 from .serializers import StatusSerializer
+
+
+def index(request):
+    statuses = Status.objects.all()
+    serializer = StatusSerializer(statuses, many=True,
+                                  context={'request': request})
+    json = JSONRenderer().render(serializer.data)
+
+    return render(request, 'base.html', {'initial': json})
+
 
 class StatusView(viewsets.ModelViewSet):
     queryset = Status.objects.all()
