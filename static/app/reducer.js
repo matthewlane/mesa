eae31ed941523cc5fa739d2f1885281ed49fdb50
Matt Lane <matthewlane@gmail.com>
2015-11-19 16:14:02 -0800

    Assign messages to state tree using shorthand object assignment

--- static/app/reducer.js
+++ static/app/reducer.js
@@ -11,21 +11,21 @@ export default function(state = {messages: []}, action) {
     return {messages: [action.message, ...state.messages]};
 
   case UPDATE_MESSAGE:
-    const newMessages = state.messages.map(candidate => {
-        if (candidate.uuid === action.message.uuid) {
-          return Object.assign({}, candidate, {
+    const messages = state.messages.map(message => {
+        if (message.uuid === action.message.uuid) {
+          return Object.assign({}, message, {
             id: action.message.id,
             url: action.message.url
           });
         }
         return candidate;
     });
-    return {messages: newMessages};
+    return {messages};
 
   case DELETE_MESSAGE:
     return {
       messages: state.messages.filter(
-        candidate => candidate.uuid !== action.message.uuid
+        message => message.uuid !== action.message.uuid
       )
     };
 
