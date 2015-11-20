97eb953be11ec58e1a139ef94d9a07f82ccf8a0e
Matt Lane <matthewlane@gmail.com>
2015-11-19 16:12:08 -0800

    Remove unneeded import

--- static/app/test/Mesa_spec.jsx
+++ static/app/test/Mesa_spec.jsx
@@ -1,6 +1,5 @@
-import {expect} from 'chai';
+import { expect } from 'chai';
 import React from 'react';
-import ReactDOM from 'react-dom';
 import TestUtils from 'react-addons-test-utils';
 import MessageList from '../components/MessageList';
 
