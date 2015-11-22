67d55a35258d27c032f787f5110d514dc37ebab5
Matt Lane <matthewlane@gmail.com>
2015-11-21 18:13:00 -0800

    Use combineReducers to manage messages slice of state

--- static/app/reducer.js
+++ static/app/reducer.js
@@ -1,35 +1,33 @@
+import { combineReducers } from 'redux';
 import {
   SET_STATE, ADD_MESSAGE, UPDATE_MESSAGE, DELETE_MESSAGE
 } from './actions';
 
-export default function(state = {messages: []}, action) {
+function messages(state = [], action) {
   switch (action.type) {
   case SET_STATE:
-    return {messages: action.state};
+    return action.state;
 
   case ADD_MESSAGE:
-    return {messages: [action.message, ...state.messages]};
+    return [action.message, ...state];
 
   case UPDATE_MESSAGE:
-    const messages = state.messages.map(message => {
+    return state.map(message => {
         if (message.uuid === action.message.uuid) {
           return Object.assign({}, message, {
             id: action.message.id,
             url: action.message.url
           });
         }
-        return candidate;
+        return message;
     });
-    return {messages};
 
   case DELETE_MESSAGE:
-    return {
-      messages: state.messages.filter(
-        message => message.uuid !== action.message.uuid
-      )
-    };
+    return state.filter(message => message.uuid !== action.message.uuid);
 
   default:
     return state;
   }
 }
+
+export default combineReducers({messages});
