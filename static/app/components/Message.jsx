455ef6b6334ecbd56d8207345506c442bea265a7
Matt Lane <matthewlane@gmail.com>
2015-11-22 22:58:11 -0800

    Display time as time ago

--- static/app/components/Message.jsx
+++ static/app/components/Message.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import moment from 'moment';
 
 export default function({message, onDelete}) {
   return (
     <div className="message">
-      <span className="text-muted">{message.created_at}</span>
+      <span className="text-muted"
+        data-toggle="tooltip"
+        data-delay="250"
+        title={moment(message.created_at).format('D MMM YYYY hh:mm A')}
+      >
+        {moment(message.created_at).fromNow()}
+      </span>
       <h2>{message.text}</h2>
       <a href="" onClick={onDelete.bind(null, message)}>Delete</a>
     </div>
