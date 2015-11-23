af9ec18c59026847d80990d2cbf023e2ccdd8976
Matt Lane <matthewlane@gmail.com>
2015-11-22 22:42:17 -0800

    Add PureRenderMixin

--- static/app/components/MessageList.jsx
+++ static/app/components/MessageList.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import PureRenderMixin from 'react-addons-pure-render-mixin';
 import Message from './Message';
 
 export default React.createClass({
+  mixins: [PureRenderMixin],
   render: function() {
     return <div>{this.props.messages.map(createMessage, this)}</div>;
     function createMessage(message) {
