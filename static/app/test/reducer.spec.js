7606c105074e467922ae115c2171b0b5043fe36e
Matt Lane <matthewlane@gmail.com>
2015-12-12 22:44:41 -0800

    Test SET_STATE without initial state

--- static/app/test/reducer.spec.js
+++ static/app/test/reducer.spec.js
@@ -24,6 +24,26 @@ describe('reducer', () => {
     });
   });
 
+  it('handles SET_STATE without initial state', () => {
+    const action = {
+      type: 'SET_STATE',
+      state: [{
+        uuid: "964074df-1ecb-4751-a16a-3be3095067de",
+        text: "ok",
+        created_at: "2015-11-13T01:29:13.189431Z"
+      }]
+    };
+    const nextState = reducer(undefined, action);
+
+    expect(nextState).to.deep.equal({
+      messages: [{
+          uuid: "964074df-1ecb-4751-a16a-3be3095067de",
+          text: "ok",
+          created_at: "2015-11-13T01:29:13.189431Z"
+      }]
+    });
+  });
+
   it('handles ADD_MESSAGE', () => {
     const initialState = {};
     const action = {
