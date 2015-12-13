5586b4863aed7cf5e4da9071d76f83207c8bc274
Matt Lane <matthewlane@gmail.com>
2015-12-12 22:29:34 -0800

    Check result of SET_STATE for deep equality

--- static/app/test/reducer.spec.js
+++ static/app/test/reducer.spec.js
@@ -3,8 +3,8 @@ import reducer from '../reducer';
 
 describe('reducer', () => {
 
-  it('sets initial state', () => {
-    const state = {messages: []};
+  it('handles SET_STATE', () => {
+    const initialState = {};
     const action = {
       type: 'SET_STATE',
       state: [{
@@ -13,10 +13,15 @@ describe('reducer', () => {
         created_at: "2015-11-13T01:29:13.189431Z"
       }]
     };
-    const nextState = reducer(state, action);
+    const nextState = reducer(initialState, action);
 
-    expect(nextState.messages.length).to.equal(1);
-    expect(nextState.messages[0]).to.equal(action.state[0]);
+    expect(nextState).to.deep.equal({
+      messages: [{
+        uuid: "964074df-1ecb-4751-a16a-3be3095067de",
+        text: "ok",
+        created_at: "2015-11-13T01:29:13.189431Z"
+      }]
+    });
   });
 
   it('stores new messages', () => {
